ff6cc438940f980e3bf8737c6daec32ab888174a
Francesca Sperati <6431@ait.nsw.edu.au>
2020-06-02 16:40:56 +1000

    test db config for heroku jawsdb

--- assets/snippets/db.php
+++ assets/snippets/db.php
@@ -1,18 +1,19 @@
 <?php
 
-  // Local Development with MAMP
-  //DEFINE('DB_USERNAME', 'root');
-  //DEFINE('DB_PASSWORD', 'root');
-  //DEFINE('DB_HOST', 'localhost');
-  //DEFINE('DB_DATABASE', 'verde');
-  //$connect = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+  if(getenv('JAWSDB_MARIA_URL')) {
+    $url = parse_url(getenv('JAWSDB_MARIA_URL'));
+    DEFINE('DB_USERNAME', $url['user']);
+    DEFINE('DB_PASSWORD', $url['pass']);
+    DEFINE('DB_HOST', $url['host']);
+    DEFINE('DB_DATABASE', substr($url["path"], 1));
+  } else {
+    DEFINE('DB_USERNAME', 'root');
+    DEFINE('DB_PASSWORD', 'root');
+    DEFINE('DB_HOST', 'localhost');
+    DEFINE('DB_DATABASE', 'verde');
+  }
 
-  // Heroku
-  $db_host = getenv('DB_HOST');
-  $db_username = getenv('DB_USERNAME');
-  $db_password = getenv('DB_PASSWORD');
-  $db_database = getenv('DB_DATABASE');
-  $connect = new mysqli($db_host, $db_username, $db_password, $db_database);
+  $connect = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
   if ($connect->connect_error)
   {
